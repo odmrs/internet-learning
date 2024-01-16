bbee65187bc2718682bb6c547790171f84c21378
odmrs <omarcosviniciusdev@gmail.com>
2024-01-16 18:10:30 -0300

    add: 2.9-comparasion and notes about comparasions between all of types

---
+++ 01-js-fundamentals/2.9-comparisons/index.js
@@ -0,0 +1,14 @@
+"use strict";
+console.clear();
+
+console.log(typeof null);
+
+// What will be the result for these expressions?
+
+// 5 > 4 -> TRUE
+// "apple" > "pineapple" -> FALSE
+// "2" > "12" -> true -> because first letter '2' is greater of '1'
+// undefined == null -> TRUE
+// undefined === null -> FALSE
+// null == "\n0\n" -> false -> null is just equals to underfined
+// null === +"\n0\n" -> FALSE
