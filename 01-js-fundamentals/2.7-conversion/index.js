55ee9699dc685003ab5fd45df57f51da079fc7be
odmrs <omarcosviniciusdev@gmail.com>
2024-01-16 15:42:18 -0300

    add: 2.7-type-conversion and notes about how convert types into other types

---
+++ 01-js-fundamentals/2.7-conversion/index.js
@@ -0,0 +1,20 @@
+"use strict";
+console.clear();
+let value = true;
+console.log(typeof value);
+
+value = String(value); // converted to string
+
+console.log(typeof value);
+
+// Number conversion
+
+let str = "123";
+console.log(typeof str);
+
+str = Number(str);
+
+console.log(typeof str);
+
+console.log(Boolean(undefined));
+console.log(Boolean("dkaoekw"));
