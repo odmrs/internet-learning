5dc342828c69e808c563bfbc9316622c6ec4c19c
odmrs <omarcosviniciusdev@gmail.com>
2024-01-16 03:08:57 -0300

    add: 2.2-code-structure about statements and comments

---
+++ 01-js-fundamentals/2.2-code-structure/index.js
@@ -0,0 +1,13 @@
+// Semicollons after statments can help us to avoid erros
+alert("Hiii");
+alert("Hiii!!"); // This is possible because the semicolon tell for the browser that line is break
+
+// But omit this semmicolons can generate errors: tell the alert is undefined
+alert("Hello");
+[(1, 2)].forEach(alert);
+
+// Comments one line 
+
+/* 
+ * comments of multiply lines
+*/
