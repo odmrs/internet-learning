f777ab07de4160bd2e9a74958d0236a823a426b6
odmrs <omarcosviniciusdev@gmail.com>
2024-01-16 15:27:09 -0300

    add: 2.6-interaction and notes about how interact with the user

---
+++ 01-js-fundamentals/2.6-interaction/index.js
@@ -0,0 +1,15 @@
+// alert("Heyy, just press the ok button!");
+//
+// let userName = prompt("Hey bro, enter your name here!", "TYPE HERE YOUR NAME");
+//
+// let fineQuestion = confirm(`Hi ${userName}, how are you bro?`);
+//
+// alert(fineQuestion);
+
+//tasksss
+//A simple page
+// importance: 4
+//
+// Create a web-page that asks for a name and outputs it.
+let userName = prompt("What's your name?");
+alert(`Hi ${userName}`);
